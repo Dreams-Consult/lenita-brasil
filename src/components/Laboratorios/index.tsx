c03bd86698e15791cda7cae57dbd257fa5e38b55
Caio Maia <caio.maiasb@gmail.com>
2026-01-03 17:27:53 -0300

    ajustes na localização dos laboratórios e nos componentes dos exames

--- src/components/Laboratorios/index.tsx
+++ src/components/Laboratorios/index.tsx
@@ -1,8 +1,11 @@
 import './index.css'
+import belemImage from '../../assets/BELEM-EDIT-1.png'
+import castanhalImage from '../../assets/CASTANHAL-EDIT-1.png'
 
 function Laboratorios() {
-  function handleLocationClick(unidade: string) {
-    alert(`Direcionando para a ${unidade}!`)
+  function handleLocationClick(address: string) {
+    const mapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`
+    window.open(mapsUrl, '_blank')
   }
 
   return (
@@ -18,11 +21,12 @@ function Laboratorios() {
         <div className='laboratorios-grid'>
           <div className='unidade-card'>
             <div className='card-image'>
+              <img src={belemImage} alt="Laboratório Unidade Belém" />
               <span className='badge'>UNIDADE BELÉM</span>
             </div>
             <div className='card-info'>
               <p className='endereco'>R. Bernal do Couto, nº 75 - Umarizal</p>
-              <button className='location-btn' onClick={() => handleLocationClick('Unidade Belém')}>
+              <button className='location-btn' onClick={() => handleLocationClick('R. Bernal do Couto, 75 - Umarizal, Belém - PA')}>
                 <span className='location-icon'>📍</span>
                 Saiba como chegar
               </button>
@@ -31,11 +35,12 @@ function Laboratorios() {
 
           <div className='unidade-card'>
             <div className='card-image'>
+              <img src={castanhalImage} alt="Laboratório Unidade Castanhal" />
               <span className='badge'>UNIDADE CASTANHAL</span>
             </div>
             <div className='card-info'>
               <p className='endereco'>Av. Marechal Deodoro, nº 368 - Ianetama</p>
-              <button className='location-btn' onClick={() => handleLocationClick('Unidade Castanhal')}>
+              <button className='location-btn' onClick={() => handleLocationClick('Av. Marechal Deodoro, 368 - Ianetama, Castanhal - PA')}>
                 <span className='location-icon'>📍</span>
                 Saiba como chegar
               </button>
